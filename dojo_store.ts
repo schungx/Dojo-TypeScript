b8ccc66d7b90028f242be3cae6e0da40d05f484a
Stephen Chung <Stephen.Chung@intexact.com>
2013-05-28 03:41:11 +0800

    Updated for latest version of 0.9

--- dojo_store.ts
+++ dojo_store.ts
@@ -1,3 +1,7 @@
+/************************************************************************/
+/* Define dojo/store package                                            */
+/************************************************************************/
+
 /// <reference path="dojo_types.ts"/>
 
 // dojo/store/api/Store
@@ -5,30 +9,30 @@
 declare class DojoStore
 {
 	idProperty: string;
-	queryEngine(query: string, options?: DojoStoreApiStoreQueryOptions): (data: any[]) => any[];
-	queryEngine(query: Object, options?: DojoStoreApiStoreQueryOptions): (data: any[]) => any[];
-	queryEngine(query: RegExp, options?: DojoStoreApiStoreQueryOptions): (data: any[]) => any[];
-	queryEngine(query: (item: Object) => boolean, options?: DojoStoreApiStoreQueryOptions): (data: any[]) => any[];
+	queryEngine(query: string, options?: _DojoStoreQueryOptions): (data: any[]) => any[];
+	queryEngine(query: Object, options?: _DojoStoreQueryOptions): (data: any[]) => any[];
+	queryEngine(query: RegExp, options?: _DojoStoreQueryOptions): (data: any[]) => any[];
+	queryEngine(query: (item: Object) => boolean, options?: _DojoStoreQueryOptions): (data: any[]) => any[];
 
-	add(object: Object, directives?: DojoStoreApiStorePutDirectives): number;
+	add(object: Object, directives?: _DojoStorePutDirectives): number;
 	get(id: number): Object;
-	getChildren(parent: Object, options?: DojoStoreApiStoreQueryOptions): DojoStoreApiStoreQueryResults;
+	getChildren(parent: Object, options?: _DojoStoreQueryOptions): DojoStoreQueryResults;
 	getIdentity(object: Object): number;
 	getMetadata(object: Object): Object;
-	put(object: Object, directives?: DojoStoreApiStorePutDirectives): number;
+	put(object: Object, directives?: _DojoStorePutDirectives): number;
 
-	query(query: string, options?: DojoStoreApiStoreQueryOptions): DojoStoreApiStoreQueryResults;
-	query(query: Object, options?: DojoStoreApiStoreQueryOptions): DojoStoreApiStoreQueryResults;
-	query(query: (item: Object) => boolean, options?: DojoStoreApiStoreQueryOptions): DojoStoreApiStoreQueryResults;
+	query(query: string, options?: _DojoStoreQueryOptions): DojoStoreQueryResults;
+	query(query: Object, options?: _DojoStoreQueryOptions): DojoStoreQueryResults;
+	query(query: (item: Object) => boolean, options?: _DojoStoreQueryOptions): DojoStoreQueryResults;
 
 	remove(id: number): boolean;
-	transaction(): DojoStoreApiStoreTransaction;
+	transaction(): DojoStoreTransaction;
 
-	PutDirectives: new ()=> DojoStoreApiStorePutDirectives;
-	QueryOptions: new ()=> DojoStoreApiStoreQueryOptions;
-	QueryResults: new ()=> DojoStoreApiStoreQueryResults;
-	SortInformation: new ()=> DojoStoreApiStoreSortInformation;
-	Transaction: new ()=> DojoStoreApiStoreTransaction;
+	PutDirectives: new ()=> _DojoStorePutDirectives;
+	QueryOptions: new ()=> _DojoStoreQueryOptions;
+	QueryResults: new ()=> DojoStoreQueryResults;
+	SortInformation: new ()=> _DojoStoreSortInformation;
+	Transaction: new ()=> DojoStoreTransaction;
 }
 
 // dojo/store/Observable
@@ -37,14 +41,14 @@ declare class DojoStoreObservable
 {
 	constructor(store: DojoStore);
 }
-interface IDojoStoreObservable
+declare module "dojo/store/Observable"
 {
-	new (store: DojoStore): DojoStoreObservable;
+	export = DojoStoreObservable;
 }
 
 // dojo/store/api/Store.PutDirectives
 
-interface DojoStoreApiStorePutDirectives
+interface _DojoStorePutDirectives
 {
 	id?: number;
 	before?: Object;
@@ -54,30 +58,30 @@ interface DojoStoreApiStorePutDirectives
 
 // dojo/store/api/Store.QueryOptions
 
-interface DojoStoreApiStoreQueryOptions
+interface _DojoStoreQueryOptions
 {
 	start?: number;
 	count?: number;
-	sort?: DojoStoreApiStoreSortInformation[];
+	sort?: _DojoStoreSortInformation[];
 }
 
 // dojo/store/api/Store.QueryResults
 
-interface DojoStoreApiStoreQueryResults
+interface DojoStoreQueryResults
 {
 	total: number;
 
-	filter(callback: (item: Object) => void , thisObject?: Object): DojoStoreApiStoreQueryResults;
-	forEach(callback: (item: Object) => void , thisObject?: Object): DojoStoreApiStoreQueryResults;
-	map(callback: (item: Object) => any, thisObject?: Object): DojoStoreApiStoreQueryResults;
-	then(callback: (items: Object[]) => void , errorHandler: (error: Object) => void ): DojoStoreApiStoreQueryResults;
+	filter(callback: (item: Object) => void , thisObject?: Object): DojoStoreQueryResults;
+	forEach(callback: (item: Object) => void , thisObject?: Object): DojoStoreQueryResults;
+	map(callback: (item: Object) => any, thisObject?: Object): DojoStoreQueryResults;
+	then(callback: (items: Object[]) => void , errorHandler: (error: Object) => void ): DojoStoreQueryResults;
 
 	observe? (listener: (object: Object, removedFrom: number, insertedInto: number) => void , includeAllUpdates?: boolean): { cancel(): void; };
 }
 
 // dojo/store/api/Store.SortInformation
 
-interface DojoStoreApiStoreSortInformation
+interface _DojoStoreSortInformation
 {
 	attribute: string;
 	descending?: boolean;
@@ -85,7 +89,7 @@ interface DojoStoreApiStoreSortInformation
 
 // dojo/store/api/Store.Transaction
 
-interface DojoStoreApiStoreTransaction
+interface DojoStoreTransaction
 {
 	abort(callback?: Function, thisObject?: Object): void;
 	commit(): void;
@@ -93,88 +97,92 @@ interface DojoStoreApiStoreTransaction
 
 // dojo/store/Memory
 
-interface DojoStoreMemoryCreateOptions
+interface _DojoStoreMemoryCreateOptions
 {
 	data?: any[];
 	idProperty?: string;
 }
-declare class DojoStoreMemory extends DojoStore implements DojoStoreMemoryCreateOptions
+declare class DojoStoreMemory extends DojoStore implements _DojoStoreMemoryCreateOptions
 {
-	constructor(options: DojoStoreMemoryCreateOptions);
+	constructor(options: _DojoStoreMemoryCreateOptions);
 
 	data: any[];
 	idProperty: string;
 	index: { [id: string]: number; };
 }
-interface IDojoStoreMemory
+declare module "dojo/store/Memory"
 {
-	new (options: DojoStoreMemoryCreateOptions): DojoStoreMemory;
+	export = DojoStoreMemory;
 };
 
 // dojo/store/DataStore
 
-interface DojoStoreDataStoreCreateOptions
+interface _DojoStoreDataStoreCreateOptions
 {
 	idProperty?: string;
 	store?: Object;		// Should be DojoDataStore, but needs to pull in dojo_data.ts and dojo.ts, so don't do it
 	target?: string;
 }
-declare class DojoStoreDataStore extends DojoStore implements DojoStoreDataStoreCreateOptions
+declare class DojoStoreDataStore extends DojoStore implements _DojoStoreDataStoreCreateOptions
 {
-	constructor(options: DojoStoreDataStoreCreateOptions);
+	constructor(options: _DojoStoreDataStoreCreateOptions);
 
 	idProperty: string;
 	store: Object;
 	target: string;
 }
-interface IDojoStoreDataStore
+declare module "dojo/store/DataStore"
 {
-	new (options: DojoStoreDataStoreCreateOptions): DojoStoreDataStore;
+	export = DojoStoreDataStore;
 }
 
 // dojo/store/JsonRest
 
-interface DojoStoreJsonRestCreateOptions
+interface _DojoStoreJsonRestCreateOptions
 {
 	accepts?: string;
 	ascendingPrefix?: string;
 	descendingPrefix?: string;
 	headers?: { [header: string]: string; };
 	idProperty?: string;
-	queryEngine(query: any, options?: DojoStoreApiStoreQueryOptions): (data: any[]) => any[];
+	queryEngine(query: any, options?: _DojoStoreQueryOptions): (data: any[]) => any[];
 	target?: string;
 }
-declare class DojoStoreJsonRest extends DojoStore implements DojoStoreJsonRestCreateOptions
+declare class DojoStoreJsonRest extends DojoStore implements _DojoStoreJsonRestCreateOptions
 {
-	constructor(options: DojoStoreJsonRestCreateOptions);
+	constructor(options: _DojoStoreJsonRestCreateOptions);
 
 	accepts: string;
 	ascendingPrefix: string;
 	descendingPrefix: string;
 	headers: { [header: string]: string; };
 	idProperty: string;
-	queryEngine(query: string, options?: DojoStoreApiStoreQueryOptions): (data: any[]) => any[];
-	queryEngine(query: Object, options?: DojoStoreApiStoreQueryOptions): (data: any[]) => any[];
-	queryEngine(query: RegExp, options?: DojoStoreApiStoreQueryOptions): (data: any[]) => any[];
-	queryEngine(query: (item: Object) => boolean, options?: DojoStoreApiStoreQueryOptions): (data: any[]) => any[];
+	queryEngine(query: string, options?: _DojoStoreQueryOptions): (data: any[]) => any[];
+	queryEngine(query: Object, options?: _DojoStoreQueryOptions): (data: any[]) => any[];
+	queryEngine(query: RegExp, options?: _DojoStoreQueryOptions): (data: any[]) => any[];
+	queryEngine(query: (item: Object) => boolean, options?: _DojoStoreQueryOptions): (data: any[]) => any[];
 	target: string;
 }
 
-interface IDojoStoreJsonRest
+declare module "dojo/store/JsonRest"
 {
-	new (options: DojoStoreJsonRestCreateOptions): DojoStoreJsonRest;
+	export = DojoStoreJsonRest;
 }
 
 // dojo/store/Observable
 
-interface DojoStoreObservable
+declare module "dojo/store/Observable"
 {
-	(store: DojoStore): DojoStore;
+	function Observable(store: DojoStore): DojoStore;
+
+	export = Observable;
 }
 
 // dojo/store/Cache
 
-interface DojoStoreCache
+declare module "dojo/store/Cache"
 {
-	(masterStore: DojoStore, cachingStore: DojoStore, options?: { isLoaded?: (item: any) => bool; }): DojoStore;
+	function Cache(masterStore: DojoStore, cachingStore: DojoStore, options?: { isLoaded?: (item: any) => bool; }): DojoStore;
+
+	export = Cache;
 }
