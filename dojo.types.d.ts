cbd90898c763241426667b392714a57da63319e4
Stephen Chung <Stephen.Chung@intexact.com>
2013-06-01 04:00:10 +0800

    Grouped types into namespaces

---
+++ dojo.types.d.ts
@@ -0,0 +1,39 @@
+/************************************************************************/
+/* Define Dojo standard types                                           */
+/************************************************************************/
+
+declare module Dojo
+{
+	// Dojo handles
+
+	export interface Handle
+	{
+	}
+	export interface RemovableHandle extends Handle
+	{
+		remove(): void;
+	}
+	export interface PausableHandle extends RemovableHandle
+	{
+		pause(): void;
+		resume(): void;
+	}
+
+	// Common callback function types
+
+	export interface BlankFunction { (): void; }
+	export interface Action { (...args: any[]): void; }
+	export interface Function { (...args: any[]): any; }
+	export interface FunctionReturning<T> { (...args: any[]): T; }
+
+	export interface ExtensionEvent { (node: HTMLElement, listener: EventListener): RemovableHandle; }
+}
+
+// AMD definitions
+
+declare function require<T>(depends: string): T;
+declare function require<T>(depends: string[], callback: Dojo.FunctionReturning<T>): T;
+
+declare function define<T>(id: string, depends: string[], callback: Dojo.FunctionReturning<T>): T;
+declare function define<T>(depends: string[], callback: Dojo.FunctionReturning<T>): T;
+declare function define(obj: Object);
