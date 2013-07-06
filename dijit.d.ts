c7080b472e6312d97fe1a32bfcedff5fdb2a6f9e
Stephen Chung <Stephen.Chung@intexact.com>
2013-07-07 00:06:49 +0800

    Enhance dojo/_base/declare

--- dijit.d.ts
+++ dijit.d.ts
@@ -4,175 +4,27 @@
 
 /// <reference path="dojo.types.d.ts"/>
 
+// Overload on declare
+
+interface _DojoDeclare
+{
+	(className: string, superclass: Dijit.WidgetOrMixin, props: Dojo.Declare.Options): Dijit._WidgetBase;
+	(className: string, superclasses: Dijit.WidgetOrMixin[], props: Dojo.Declare.Options): Dijit._WidgetBase;
+	(superclass: Dijit.WidgetOrMixin, props: Dojo.Declare.Options): Dijit._WidgetBase;
+	(superclasses: Dijit.WidgetOrMixin[], props: Dojo.Declare.Options): Dijit._WidgetBase;
+}
+
+// dijit
+
 declare module Dijit
 {
-	// dijit/_WidgetBase
+	interface _Mixin extends WidgetOrMixin { }
 
-	class _WidgetBase extends _Widget
+	// dijit/_Widget
+
+	class _Widget extends _WidgetBase implements _FocusMixin
 	{
-		// _Widget
-		"get"(name: "baseClass"): string;
-		"get"(name: "class"): string;
-		"get"(name: "containerNode"): HTMLElement;
-		"get"(name: "dir"): string;
-		"get"(name: "domNode"): HTMLElement;
-		"get"(name: "focused"): boolean;
-		"get"(name: "id"): string;
-		"get"(name: "lang"): string;
-		"get"(name: "ownerDocument"): HTMLDocument;
-		"get"(name: "postMixInProperties"): string;
-		"get"(name: "srcNodeRef"): HTMLElement;
-		"get"(name: "style"): Dojo.StylesMap;
-		"get"(name: "title"): string;
-		"get"(name: "tooltip"): string;
-		"get"(name: string): any;
-
-		"set"(name: "baseClass", value: string): void;
-		"set"(name: "class", value: string): void;
-		"set"(name: "containerNode", value: HTMLElement): void;
-		"set"(name: "dir", value: string): void;
-		"set"(name: "domNode", value: HTMLElement): void;
-		"set"(name: "focused", value: boolean): void;
-		"set"(name: "id", value: string): void;
-		"set"(name: "lang", value: string): void;
-		"set"(name: "ownerDocument", value: HTMLDocument): void;
-		"set"(name: "postMixInProperties", value: string): void;
-		"set"(name: "srcNodeRef", value: HTMLElement): void;
-		"set"(name: "style", value: Dojo.StylesMap): void;
-		"set"(name: "title", value: string): void;
-		"set"(name: "tooltip", value: string): void;
-		"set"(name: string, value: any): void;
-		"set"(values: Dojo.PropertiesMap): void;
-
-		watch(prop: "baseClass", callback: Dojo.WatchCallback<string>): Dojo.WatchHandle;
-		watch(prop: "class", callback: Dojo.WatchCallback<string>): Dojo.WatchHandle;
-		watch(prop: "containerNode", callback: Dojo.WatchCallback<HTMLElement>): Dojo.WatchHandle;
-		watch(prop: "dir", callback: Dojo.WatchCallback<string>): Dojo.WatchHandle;
-		watch(prop: "domNode", callback: Dojo.WatchCallback<HTMLElement>): Dojo.WatchHandle;
-		watch(prop: "focused", callback: Dojo.WatchCallback<boolean>): Dojo.WatchHandle;
-		watch(prop: "id", callback: Dojo.WatchCallback<string>): Dojo.WatchHandle;
-		watch(prop: "lang", callback: Dojo.WatchCallback<string>): Dojo.WatchHandle;
-		watch(prop: "ownerDocument", callback: Dojo.WatchCallback<HTMLDocument>): Dojo.WatchHandle;
-		watch(prop: "postMixInProperties", callback: Dojo.WatchCallback<string>): Dojo.WatchHandle;
-		watch(prop: "srcNodeRef", callback: Dojo.WatchCallback<HTMLElement>): Dojo.WatchHandle;
-		watch(prop: "style", callback: Dojo.WatchCallback<Dojo.StylesMap>): Dojo.WatchHandle;
-		watch(prop: "title", callback: Dojo.WatchCallback<string>): Dojo.WatchHandle;
-		watch(prop: "tooltip", callback: Dojo.WatchCallback<string>): Dojo.WatchHandle;
-		watch(prop: string, callback: Dojo.WatchCallback<any>): Dojo.WatchHandle;
-
-
-		buildRendering(): void;
-
-		/* Deprecated
-				connect(obj: Object, event: string, method: string): Dojo.Handle;
-				connect(obj: Object, event: Dojo.ExtensionEvent, method: string): Dojo.Handle;
-				connect(obj: Object, event: string, method: EventListener): Dojo.Handle;
-				connect(obj: Object, event: Dojo.ExtensionEvent, method: EventListener): Dojo.Handle;
-
-				disconnect(handle: Dojo.Handle): void;
-		*/
-
-		defer(fcn: Dojo.Action, delay: number): Dojo.RemovableHandle;
-		destroy(preserveDom?: boolean): void;
-		destroyDescendants(preserveDom?: boolean): void;
-		destroyRecursive(preserveDom?: boolean): void;
-		destroyRendering(preserveDom?: boolean): void;
-		emit(type: string, eventObj: Object, callbackArgs?: any[]): void;
-
-		getChildren(): _WidgetBase[];
-		getParent(): _WidgetBase;
-		isFocusable(): boolean;
-		isLeftToRight(): boolean;
-		isValid(): boolean;
-
-		on(type: "abort", listener: (ev: UIEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "afterprint", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "beforeprint", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "beforeunload", listener: (ev: BeforeUnloadEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "blur", listener: (ev: FocusEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "canplay", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "canplaythrough", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "change", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "click", listener: (ev: MouseEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "contextmenu", listener: (ev: MouseEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "dblclick", listener: (ev: MouseEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "drag", listener: (ev: DragEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "dragend", listener: (ev: DragEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "dragenter", listener: (ev: DragEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "dragleave", listener: (ev: DragEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "dragover", listener: (ev: DragEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "dragstart", listener: (ev: DragEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "drop", listener: (ev: DragEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "durationchange", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "emptied", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "ended", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "focus", listener: (ev: FocusEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "hashchange", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "input", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "keydown", listener: (ev: KeyboardEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "keypress", listener: (ev: KeyboardEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "keyup", listener: (ev: KeyboardEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "load", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "loadeddata", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "loadedmetadata", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "loadstart", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "message", listener: (ev: MessageEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "mousedown", listener: (ev: MouseEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "mousemove", listener: (ev: MouseEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "mouseout", listener: (ev: MouseEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "mouseover", listener: (ev: MouseEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "mouseup", listener: (ev: MouseEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "mousewheel", listener: (ev: MouseWheelEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "offline", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "online", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "pause", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "play", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "playing", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "progress", listener: (ev: any) => boolean): Dojo.RemovableHandle;
-		on(type: "ratechange", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "readystatechange", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "reset", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "resize", listener: (ev: UIEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "scroll", listener: (ev: UIEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "seeked", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "seeking", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "select", listener: (ev: UIEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "stalled", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "storage", listener: (ev: StorageEvent) => boolean): Dojo.RemovableHandle;
-		on(type: "submit", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "suspend", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "timeupdate", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "unload", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "volumechange", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: "waiting", listener: (ev: Event) => boolean): Dojo.RemovableHandle;
-		on(type: string, listener: Dojo.Action): Dojo.RemovableHandle;
-		on(type: Dojo.ExtensionEvent, func: Dojo.Action): Dojo.RemovableHandle;
-
-		own(handle: Dojo.RemovableHandle): Dojo.RemovableHandle[];
-
-		placeAt(referenceId: string, position: string): _WidgetBase;
-		placeAt(referenceNode: HTMLElement, position: string): _WidgetBase;
-		placeAt(referenceWidget: _WidgetBase, position: string): _WidgetBase;
-		placeAt(referenceId: string, position: number): _WidgetBase;
-		placeAt(referenceNode: HTMLElement, position: number): _WidgetBase;
-		placeAt(referenceWidget: _WidgetBase, position: number): _WidgetBase;
-
-		postCreate(): void;
-		startup(): void;
-		toString(): string;
-
-		/* Deprecated
-				subscribe(topic: string, callback: Dojo.Action): Dojo.Handle;
-				uninitialize(): boolean;
-				unsubscribe(handle: Dojo.Handle): void;
-		*/
-	}
-
-	// dijit/Widget
-
-	class Widget extends _WidgetBase implements _FocusMixin
-	{
-		// _WidgetBase
+		// dijit/_WidgetBase
 		"get"(name: "baseClass"): string;
 		"get"(name: "class"): string;
 		"get"(name: "containerNode"): HTMLElement;
@@ -245,13 +97,13 @@ declare module Dijit
 
 	// Common widget mixin's
 
-	interface _FocusMixin
+	interface _FocusMixin extends _Mixin
 	{
 		onBlur(): void;
 		onFocus(): void;
 	}
 
-	interface _Container
+	interface _Container extends _Mixin
 	{
 		addChild(widget: _WidgetBase, insertIndex?: number): void;
 		getIndexOfChild(child: _WidgetBase): number;
@@ -260,14 +112,14 @@ declare module Dijit
 		removeChild(widget: number): void;
 	}
 
-	interface _Contained
+	interface _Contained extends _Mixin
 	{
 		getIndexInParent(): number;
 		getNextSibling(): _WidgetBase;
 		getPreviousSibling(): _WidgetBase;
 	}
 
-	interface _AttachMixin
+	interface _AttachMixin extends _Mixin
 	{
 		//attachScope: Object;
 		//searchContainerNode: boolean;
@@ -342,7 +194,7 @@ declare module Dijit
 	{
 	}
 
-	interface _CssStateMixin
+	interface _CssStateMixin extends _Mixin
 	{
 		//active: boolean;
 		//cssStateNodes: { [attachPoint: string]: string; };
@@ -376,11 +228,6 @@ declare module "dijit/_WidgetBase"
 {
 	var _WidgetBase: Dijit._WidgetBase;
 	export = _WidgetBase;
-}
-declare module "dijit/Widget"
-{
-	var Widget: Dijit.Widget;
-	export = Widget;
 }
 declare module "dijit/_FocusMixin"
 {
@@ -470,7 +317,7 @@ declare module Dijit
 {
 	module Form
 	{
-		interface _FormMixin
+		interface _FormMixin extends _Mixin
 		{
 			//state: string;
 
@@ -488,4 +335,12 @@ declare module Dijit
 			validate(): void;
 		}
 	}
+}
+
+// dijit/form module definitions
+
+declare module "dijit/form/_FormMixin"
+{
+	var _FormMixin: Dijit.Form._FormMixin;
+	export = _FormMixin;
 }
